6838d03102fda920708e3e34f1c0d095683b8c95
ErickErickErickErick <erick_braatz@estudante.sesisenai.org.br>
2023-05-16 12:22:48 -0300

    a

--- CAIXAS.js
+++ CAIXAS.js
@@ -10,3 +10,32 @@ function replaceProfile(image)
 {
     profile.src=image;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const carouselContainer = document.querySelector('.carousel-container');
+    const prevButton = document.querySelector('.carousel-prev');
+    const nextButton = document.querySelector('.carousel-next');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const totalItems = carouselItems.length;
+    let currentIndex = 0;
+  
+    function showSlide(index) {
+      if (index < 0 || index >= totalItems) {
+        return;
+      }
+  
+      carouselContainer.style.transform = `translateX(-${index * 100}%)`;
+      currentIndex = index;
+    }
+  
+    function showNextSlide() {
+      showSlide(currentIndex + 1);
+    }
+  
+    function showPrevSlide() {
+      showSlide(currentIndex - 1);
+    }
+  
+    nextButton.addEventListener('click', showNextSlide);
+    prevButton.addEventListener('click', showPrevSlide);
+  });
