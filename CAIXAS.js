35b7215bef61433275d4876af88a41addc46194f
ErickErickErickErick <erick_braatz@estudante.sesisenai.org.br>
2023-05-17 09:35:07 -0300

    a

--- CAIXAS.js
+++ CAIXAS.js
@@ -10,32 +10,59 @@ function replaceProfile(image)
 {
     profile.src=image;
 }
+function replaceBox(image)
+{
+    box.src=image;
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    const carouselContainer = document.querySelector('.carousel-container');
-    const prevButton = document.querySelector('.carousel-prev');
-    const nextButton = document.querySelector('.carousel-next');
-    const carouselItems = document.querySelectorAll('.carousel-item');
-    const totalItems = carouselItems.length;
-    let currentIndex = 0;
-  
-    function showSlide(index) {
-      if (index < 0 || index >= totalItems) {
-        return;
-      }
-  
-      carouselContainer.style.transform = `translateX(-${index * 100}%)`;
-      currentIndex = index;
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+//carrossel//
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+//filtro//
+function clickBtn() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
     }
-  
-    function showNextSlide() {
-      showSlide(currentIndex + 1);
-    }
-  
-    function showPrevSlide() {
-      showSlide(currentIndex - 1);
-    }
-  
-    nextButton.addEventListener('click', showNextSlide);
-    prevButton.addEventListener('click', showPrevSlide);
-  });
+  }
+}
